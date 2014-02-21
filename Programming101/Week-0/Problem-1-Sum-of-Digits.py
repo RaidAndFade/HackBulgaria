22c469dc04eade7566f8e98acb8cf67e540dc3c2
syndbg <syndbe@gmail.com>
2014-02-21 16:06:07 +0200

    init

---
+++ Programming101/Week-0/Problem-1-Sum-of-Digits.py
@@ -0,0 +1,59 @@
+# Problem 1
+#
+# Given an integer, write a function,
+# called sum_of_digits(n) that calculates
+# the sum of the digits of n.
+#
+# If a negative number is given, the function
+# should work as if it was positive.
+#
+# For example, if n is 1325132435356, the digit's sum is 43.
+#  If n is -10, the sum is 1 + 0 = 1
+#
+# Keep in mind that in Python, there is a special operator
+# for integer division:
+#
+#     >>> 5 / 2
+#     2.5
+#     >>> 5 // 2
+#     2
+#
+# Signature
+#
+#     def sum_of_digits(n):
+#         # implementation
+#
+# Test examples
+#
+#     >>> sum_of_digits(1325132435356)
+#     43
+#     >>> sum_of_digits(123)
+#     6
+#     >>> sum_of_digits(6)
+#     6
+#     >>> sum_of_digits(-10)
+#     1
+
+# FUNCTIONS
+def sum_of_digits(number):
+    outputSum = 0
+
+    if number < 1:
+        number *= -1
+
+    numDigits = len(str(number))
+
+    for i in range(numDigits):
+        digit = number % 10
+        outputSum += digit
+        number = number // 10
+
+    return outputSum
+
+# main
+def main():
+    inputN = int(input("Enter a number: "))
+    print("Sum of digits:", sum_of_digits(inputN))
+
+# PROGRAM RUN
+main()
