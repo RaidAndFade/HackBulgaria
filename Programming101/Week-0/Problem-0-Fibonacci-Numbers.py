4cfc38836fd65632256de598e35bbad845a34b6c
syndbg <syndbe@gmail.com>
2014-02-21 15:57:10 +0200

    init

---
+++ Programming101/Week-0/Problem-0-Fibonacci-Numbers.py
@@ -0,0 +1,40 @@
+# Problem 0
+#
+#     The most annoying problem of all. Write a function,
+#     called nth_fibonacci(n) that returns the n-th fibonacci number,
+#     given by the argument.
+#
+# Signature
+#
+#     def nth_fibonacci(n):
+#         #implementation here
+#
+# Test examples
+#
+#     >>> nth_fibonacci(1)
+#     1
+#     >>> nth_fibonacci(2)
+#     1
+#     >>> nth_fibonacci(3)
+#     2
+#     >>> nth_fibonacci(10)
+#     55
+
+
+# FUNCTIONS
+def nth_fibonacci(number):
+    if number <= 2:
+        return 1
+
+    else:
+        return nth_fibonacci(number - 1) + nth_fibonacci(number - 2)
+
+
+# main
+def main():
+    inputN = int(input("Enter a number: "))
+
+    print("Fibonacci result:", nth_fibonacci(inputN))
+
+# PROGRAM RUN
+main()
