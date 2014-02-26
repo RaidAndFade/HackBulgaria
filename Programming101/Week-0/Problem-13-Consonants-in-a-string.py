153510f1ab6048e467ad01dc3196a426eb5a607d
syndbg <syndbe@gmail.com>
2014-02-27 00:39:12 +0200

    updated as there was a "p" missing in consonants check

--- Programming101/Week-0/Problem-13-Consonants-in-a-string.py
+++ Programming101/Week-0/Problem-13-Consonants-in-a-string.py
@@ -24,7 +24,7 @@
 
 # FUNCTIONS
 def count_consonants(string):
-    consonants = ["b", "c", "d", "f", "g", "h", "j", "k", "l", "m", "n", "q", "r", "s", "t", "v", "w", "x", "z" ]
+    consonants = ["b", "c", "d", "f", "g", "h", "j", "k", "l", "m", "n", "p", "q", "r", "s", "t", "v", "w", "x", "z" ]
 
     numConsonants = 0
 
