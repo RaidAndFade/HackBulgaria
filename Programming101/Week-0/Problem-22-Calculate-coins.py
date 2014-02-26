4ebd16d6e660517559fd6222afb2f9d531681edd
syndbg <syndbe@gmail.com>
2014-02-27 00:03:37 +0200

    init

--- Programming101/Week-0/Problem-22-Calculate-coins.py
+++ Programming101/Week-0/Problem-22-Calculate-coins.py
@@ -21,21 +21,18 @@
 # >>> calculate_coins(8.94)
 # {1: 0, 2: 2, 100: 8, 5: 0, 10: 0, 50: 1, 20: 2}
 
-# IMPORTS
-import math
-
 # FUNCTIONS
 def calculate_coins(money):
-    COINS = [ 50, 20, 10, 5, 2, 1 ]
-    result = {}
-    result[100] = math.trunc(money)
-    remainder = round(math.modf(money)[0] * 100)
+    # money *= 100
+    COINS = [ 100, 50, 20, 10, 5, 2, 1 ]
+    outputList = ()
 
     for coin in COINS:
-        result[coin] = round(math.trunc(remainder / coin))
-        remainder = round(remainder - result[coin] * coin)
+        coinsNeeded = int(money / coin)
+        outputList += (coin, ) * coinsNeeded
+        money -= coin * coinsNeeded
 
-    return result
+    return outputList
 
 # main
 def main():
