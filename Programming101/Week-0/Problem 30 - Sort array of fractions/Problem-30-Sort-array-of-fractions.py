60c55f730f0fbbcb58046a3a312e67d1f7b4d8d8
syndbg <syndbe@gmail.com>
2014-03-04 15:42:32 +0200

    restructuring folders

---
+++ Programming101/Week-0/Problem 30 - Sort array of fractions/Problem-30-Sort-array-of-fractions.py
@@ -0,0 +1,32 @@
+# Problem 30 - Sort array of fractions
+
+# Implement a function, called sort_fractions(fractions) 
+# where fractions is a list of tuples of the form (nominator, denominator).
+# Both the nominator and the denominator are integers.
+# The function should return the list, sorted in increasing order.
+
+# Signature
+
+# def sort_fractions(fractions):
+#     # Implementation
+    
+# Test examples
+
+#     >>> sort_fractions([(2, 3), (1, 2)])
+#     [(1, 2), (2, 3)]
+#     >>> sort_fractions([(2, 3), (1, 2), (1, 3)])
+#     [(1, 3), (1, 2), (2, 3)]
+#     >>> sort_fractions([(5, 6), (22, 78), (22, 7), (7, 8), (9, 6), (15, 32)])
+#     [(22, 78), (15, 32), (5, 6), (7, 8), (9, 6), (22, 7)]
+
+# IMPORTS
+
+# FUNCTIONS
+
+# main
+def main():
+    
+    
+# PROGRAM RUN
+if __name__ == "__main__":
+    main()
