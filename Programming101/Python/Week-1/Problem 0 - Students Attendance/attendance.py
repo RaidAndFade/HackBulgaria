35849311e6339613d9f329c7e45da396e8b2cf8e
syndbg <syndbe@gmail.com>
2014-03-09 18:08:06 +0200

    :zap: Refactored attendance.py and fixed trigger_command_statistic. trigger_command_statistic: No need to trigger list explicitly beforehand. Now, updates files list before outputting stats.

--- Programming101/Python/Week-1/Problem 0 - Students Attendance/attendance.py
+++ Programming101/Python/Week-1/Problem 0 - Students Attendance/attendance.py
@@ -20,28 +20,31 @@ def is_command(command_tuple, command_string):
     return command_tuple[0] == command_string
 
 
-def trigger_command_add(command_tuple):
+def trigger_command_add(command_tuple, attending_students):
     if len(command_tuple) != 3:
         print("Please enter student as: FirstName LastName")
         return
 
     student = "%s %s" % (command_tuple[1], command_tuple[2])
-    file = open("students", "r")
-    for line in file:
+    students_file = open("students", "r")
+    for line in students_file:
         if student in attending_students:
             print("%s is already attending!" % student)
+            students_file.close()
             return
 
         if student in line:
             print("%s is now attending!" % student)
             attending_students.append(student)
+            students_file.close()
             return True
 
-    print("%s is not in our records" % student)
+    students_file.close()
+    print("%s is not in our records." % student)
     return False
 
 
-def trigger_command_create():
+def trigger_command_create(current_date, attending_students):
     filename = "attendance_%s" % current_date
     #current_open_file = "attendance_%s" % current_date
 
@@ -53,30 +56,36 @@ def trigger_command_create():
     print("New file created and loaded:", filename)
 
 
-def trigger_command_change_date(command_tuple):
+def trigger_command_change_date(command_tuple, current_date):
     # format current_date for trigger_command_create
-    global current_date
     current_date = "%s_%s_%s" % (command_tuple[3], command_tuple[2], command_tuple[1])
-    print("Changed current date to %s/%s/%s" % (command_tuple[1], command_tuple[2], command_tuple[3]))
+    print("Changed current date to %s/%s/%s" %
+          (command_tuple[1], command_tuple[2], command_tuple[3]))
+    return current_date
 
 
-def trigger_command_list():
+def trigger_command_list(files):
+    list_output = []
     i = 0
     for file in os.listdir('.'):
         if file.startswith("attendance_"):
             i += 1
             files.append(file)
-            print("[%s] - %s" % (i, file))
+            # print("[%s] - %s" % (i, file))
+            list_output.append("[%s] - %s" % (i, file))
+
+    # update files to current listed
+    files = list_output
+    return "\n".join(list_output)
 
 
-def trigger_command_load(command_tuple):
+def trigger_command_load(command_tuple, files, attending_students):
     if len(command_tuple) != 2:
         print("Error: Invalid number of arguments given!")
         return
 
-    current_open_file = files[int(command_tuple[1])-1]
+    current_open_file = files[int(command_tuple[1]) - 1]
     print("File %s loaded!" % current_open_file)
-    global attending_students
     # clear list
     attending_students[:] = []
 
@@ -84,31 +93,45 @@ def trigger_command_load(command_tuple):
     for line in file:
         attending_students.append(line.rstrip())
     file.close()
+
     return
 
 
-def trigger_command_status():
+def trigger_command_status(attending_students):
+    list_output = []
     for i in range(len(attending_students)):
         # it's.. fancy!
         fancy_index = i + 1
-        print("%s. %s" % (fancy_index, attending_students[i]))
+        # print("%s. %s" % (fancy_index, attending_students[i]))
+        list_output.append("%s. %s" % (fancy_index, attending_students[i]))
+
+    return "\n".join(list_output)
 
 
-def trigger_command_statistic():
+def trigger_command_statistic(files):
     # get total registered
     total_registered_students = 0
     students_file = open("students", "r")
     for line in students_file:
         total_registered_students += 1
 
-    # bug: must have used list before statistic or else it doesn't work.
-    for file in files:
-        file = open(file, "r")
+    # FIXED! bug: must have used list before statistic or else it doesn't work.
+    list_output = []
+    # update files list before getting stats
+    trigger_command_list(files)
+
+    for filename in files:
+        file = open(filename, "r")
         file_students_attending = 0
         for line in file:
             file_students_attending += 1
-        print("File: %s - %s attending from total of %s students" % (file, file_students_attending, total_registered_students))
+        stats = "File: %s - %s attending from total of %s students" % (
+            filename, file_students_attending, total_registered_students)
+
+        list_output.append(stats)
         file.close()
+
+    return "\n".join(list_output)
 
 
 def trigger_unknown_command():
@@ -116,46 +139,44 @@ def trigger_unknown_command():
                        "Available commands are:",
                        "create", "change_date",
                        "add", "list", "load",
-                       "status", "statistics"]
+                       "status", "statistic"]
 
     return "\n".join(unknown_command)
 
 
-# Must be global!
-# Stores date (current date unless modified)
-ts = time()
-current_date = datetime.fromtimestamp(ts).strftime('%Y_%m_%d')
-# Current loaded file's attending students
-attending_students = []
-# All files in directory. Will be updated only if list is used!
-files = []
-
-
 # main
 def main():
+    # Stores date (current date unless modified)
+    ts = time()
+    current_date = datetime.fromtimestamp(ts).strftime('%Y_%m_%d')
+    # Current loaded file's attending students
+    attending_students = []
+    # All files in directory. Will be updated only if list is used!
+    files = []
+
     while True:
         command = parse_command(input("Enter a command> "))
 
         if is_command(command, "create"):
-            trigger_command_create()
+            trigger_command_create(current_date, attending_students)
 
         elif is_command(command, "change_date"):
-            trigger_command_change_date(command)
+            current_date = trigger_command_change_date(command, current_date)
 
         elif is_command(command, "add"):
-            trigger_command_add(command)
+            trigger_command_add(command, attending_students)
 
         elif is_command(command, "list"):
-            trigger_command_list()
+            print(trigger_command_list(files))
 
         elif is_command(command, "load"):
-            trigger_command_load(command)
+            trigger_command_load(command, files, attending_students)
 
         elif is_command(command, "status"):
-            trigger_command_status()
+            print(trigger_command_status(attending_students))
 
         elif is_command(command, "statistic"):
-            trigger_command_statistic()
+            print(trigger_command_statistic(files))
 
         else:
             print(trigger_unknown_command())
